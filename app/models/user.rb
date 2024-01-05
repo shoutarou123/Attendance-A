64f503510d0bc9d9e10a44d6b92096839bae0cdf
shoutaro <850123.a@gmail.com>
2024-01-05 15:11:52 +0900

    4.4.5終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i # 正規表現
+  validates :email, presence: true, length: { maximum: 100 },
+                    format: { with: VALID_EMAIL_REGEX } # 正規表現をformatにしている
+                    uniqueness: true # 一意性検証
 end
