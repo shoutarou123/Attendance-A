8bb84eafc18bf852c73b70c464cfab4e48336500
shoutaro <850123.a@gmail.com>
2024-01-06 00:02:52 +0900

    5章 Finish signup-user終了 dbﾘｾｯﾄできない症状あり

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user # redirect_to user_url(@user)と同じ意味
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
