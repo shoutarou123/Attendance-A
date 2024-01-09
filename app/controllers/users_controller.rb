b66c96215e75c9e439ef8eeee6cc7a9e6c99421c
shoutaro <850123.a@gmail.com>
2024-01-09 16:15:10 +0900

    8.1.2終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # 更新に成功した場合の処理を記述します。
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  end
   private
 
     def user_params
