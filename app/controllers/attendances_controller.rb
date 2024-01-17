73db0bbe7c0f073334cb054769f4f91b1f340881
shoutaro <850123.a@gmail.com>
2024-01-17 17:34:13 +0900

    10.5 終了

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,2 +1,15 @@
 class AttendancesController < ApplicationController
+
+  def update
+    @user = User.find(params[:user_id])
+    @attendance = Attendance.find(params[:id])
+    if @attendance.started_at.nil? # 出社が未登録であること
+     if @attendance.update(started_at: Time.current.change(sec: 0)) # 秒数を0指定
+      flash[:info] = "出社時間を登録しました。"
+     else
+      flash[:danger] = "出社時間登録に失敗しました。やり直してください。"
+     end
+    end
+    redirect_to @user
+  end
 end
