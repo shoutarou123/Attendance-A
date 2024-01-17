58f73a3ab5acb8c9c1f472d99931a11ddc222e9f
shoutaro <850123.a@gmail.com>
2024-01-17 16:24:49 +0900

    10.4 終了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,19 @@ class ApplicationController < ActionController::Base
   $days_of_the_week = %w{日 月 火 水 木 金 土}
 
   def set_one_month  # ﾍﾟｰｼﾞ出力前に1ヶ月分のﾃﾞｰﾀの存在を確認・ｾｯﾄします。
-    @first_day = Date.current.beginning_of_month
+    @first_day = params[:date].nil? ? # dateが無い時本日月初を代入し、dateが有る時、その日を代入。
+    Date.current.beginning_of_month : params[:date].to_date
     @last_day = @first_day.end_of_month
     one_month = [*@first_day..@last_day] # 対象の月の日数を代入します showｱｸｼｮﾝでは使わない為ﾛｰｶﾙ変数に代入している
-    @attendances = @user.attendances.where(worked_on: @first_day..@last_day)  # ﾕｰｻﾞｰに紐付く1ヶ月分のﾚｺｰﾄﾞを検索し取得します。
+    
+    @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)  # ﾕｰｻﾞｰに紐付く1ヶ月分のﾚｺｰﾄﾞを検索し取得します。
     # attendancesを複数形としていのはActivbeRecord特有の記法で、対象のﾓﾃﾞﾙ（今回はUserﾓﾃﾞﾙ）に紐付くﾓﾃﾞﾙという意味。UserﾓﾃﾞﾙとAttendanceﾓﾃﾞﾙは1対多の関係なので、attendancesとなる。
+    # order(:worked_on)で昇順にしている。
     unless one_month.count == @attendances.count # 1ヶ月分の日付の件数と勤怠ﾃﾞｰﾀの件数が一致しない時
       ActiveRecord::Base.transaction do # ﾄﾗﾝｻﾞｸｼｮﾝを開始
         one_month.each { |day| @user.attendances.create!(worked_on: day) } # 繰り返し処理により、1ヶ月分の勤怠ﾃﾞｰﾀを生成します。
       end
+      @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
     end
 
   rescue ActiveRecord::RecordInvalid # ﾄﾗﾝｻﾞｸｼｮﾝによるｴﾗｰの分岐です。
