3080b3d438c4b80dc898c0b6793016cbf0eb182f
shoutaro <850123.a@gmail.com>
2024-01-17 22:11:08 +0900

    10.8 終了

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -5,7 +5,10 @@ module AttendancesHelper
       return '出社' if attendance.started_at.nil? # 出社が無ければ出社を返す
       return '退社' if attendance.started_at.present? && attendance.finished_at.nil? # 出社が存在かつ退社が無ければ退社を返す
     end
+    return false # # どれにも当てはまらなかった場合はfalseを返します。
+  end
 
-    false
+  def working_times(start, finish) # 出勤時間と退勤時間を受け取り、在社時間を計算して返します
+    format("%02d:%02d", ((finish - start) / 3600).to_i, ((finish - start) % 3600 / 60).to_i)
   end
 end
