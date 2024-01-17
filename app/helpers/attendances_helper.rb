0661e317d5f7f4b346910fa840e3280df25bb298
shoutaro <850123.a@gmail.com>
2024-01-17 21:13:25 +0900

    10.6.1 終了

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,2 +1,11 @@
 module AttendancesHelper
+  
+  def attendance_state(attendance)
+    if Date.current == attendance.worked_on # 受け取ったAttendanceｵﾌﾞｼﾞｪｸﾄが当日と一致する場合
+      return '出社' if attendance.started_at.nil? # 出社が無ければ出社を返す
+      return '退社' if attendance.started_at.present? && attendance.finished_at.nil? # 出社が存在かつ退社が無ければ退社を返す
+    end
+
+    false
+  end
 end
