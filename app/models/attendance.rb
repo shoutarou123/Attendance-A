f6dfadb24d7505fc6742c052eda29a49bed9e29f
shoutaro <850123.a@gmail.com>
2024-01-17 11:36:49 +0900

    10.1.3終了 出勤時間なしﾊﾞﾘﾃﾞｰｼｮﾝﾒｯｾｰｼﾞ表示ならず

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,4 +3,10 @@ class Attendance < ApplicationRecord
 
   validates :worked_on, presence: true
   validates :note, length: { maximum: 50 }
+
+  validate :finished_at_is_invalid_without_a_started_at # 出勤時間が存在しない場合、退勤時間は無効
+
+  def finished_at_is_invalid_without_a_started_at
+    errors.add(:started_at, "が必要です") if started_at_blank? && finished_at_present? # 出勤空かつ退勤存在の時、ｴﾗｰ表示追加
+  end
 end
